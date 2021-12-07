8b504a4ad21b55d2c93152a85687d81202f141a3
Davi Lacerda <dlacerda9@gmail.com>
2021-12-07 19:37:11 -0300

    change style and improved javascript

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ var [bmi, weight, height] = [0, 0, 0];
 weightInput.addEventListener('input', () => {
     weight = weightInput.value;
 
+    if(weightInput.value > 500){
+        alert('Weight Max : 500 kg')
+        weightInput.value = 500;
+    }
+
+    weightRange.value = weightInput.value;
     bmiChange();
 });
 
@@ -28,6 +34,12 @@ weightRange.addEventListener('input', () => {
 heightInput.addEventListener('input', () => {
     height = heightInput.value / 100;
 
+    if(heightInput.value > 250){
+        alert('Height Max : 250 cm')
+        heightInput.value = 250;
+    }
+
+    heightRange.value = heightInput.value;
     bmiChange();
 });
 
@@ -49,27 +61,26 @@ function bmiChange() {
 }
 
 function bmiClass(param) {
+    if(!isNaN(bmi)){
+        bmi_class.style.display = 'flex';
+    }
+
     if(param < 18.5){
-        bmi_class.innerHTML = 'Abaixo do Peso'
+        bmi_class.innerHTML = 'Underweight'
     }
     else if(param <= 24.9){
-        bmi_class.style.display = 'flex';
-        bmi_class.innerHTML = 'Peso Normal'
+        bmi_class.innerHTML = '	Healthy Weight'
     }
     else if(param <= 29.9){
-        bmi_class.style.display = 'flex';
-        bmi_class.innerHTML = 'Sobrepeso'
+        bmi_class.innerHTML = 'Overweight'
     }
     else if(param <= 34.9){
-        bmi_class.style.display = 'flex';
-        bmi_class.innerHTML = 'Obesidade Grau I'
+        bmi_class.innerHTML = 'Obesity Class I'
     }
     else if(param <= 39.9){
-        bmi_class.style.display = 'flex';
-        bmi_class.innerHTML = 'Obesidade Grau II'
+        bmi_class.innerHTML = 'Obesity Class II'
     }
     else{
-        bmi_class.style.display = 'flex';
-        bmi_class.innerHTML = 'Obesidade Grau III'
+        bmi_class.innerHTML = 'Obesity Class III'
     }
 }
